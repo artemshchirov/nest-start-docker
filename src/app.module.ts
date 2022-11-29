ddb2223ea6456ecef5c9f4a81d9c0c1533b63300
Artem Shchirov <artemshchirov@gmail.com>
2022-11-29 14:48:02 +0200

    feat: add roles module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule } from "@nestjs/config";
 import { SequelizeModule } from '@nestjs/sequelize';
 import { User } from "./users/users.model";
 import { UsersModule } from './users/users.module';
+import { RolesModule } from './roles/roles.module';
+import { Role } from "./roles/roles.model";
 
 
 @Module({
@@ -19,10 +21,11 @@ import { UsersModule } from './users/users.module';
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
-      models: [User],
+      models: [User, Role],
       autoLoadModels: true
     }),
     UsersModule,
+    RolesModule,
   ]
 })
 
---
+++ src/roles/roles.model.ts
@@ -0,0 +1,23 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { Column, DataType, Model, Table } from "sequelize-typescript";
+
+interface RoleCreationAttrs {
+  value: string;
+  description: string;
+}
+
+@Table({ tableName: 'roles' })
+export class Role extends Model<Role, RoleCreationAttrs> {
+
+  @ApiProperty({ example: '1', description: 'Unique id' })
+  @Column({ type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true })
+  id: number;
+
+  @ApiProperty({ example: 'ADMIN', description: 'Unique user role value' })
+  @Column({ type: DataType.STRING, unique: true, allowNull: false })
+  value: string;
+
+  @ApiProperty({ example: 'Administrator', description: 'Role description' })
+  @Column({ type: DataType.STRING, allowNull: false })
+  description: string;
+}
